791020e75b65b4b51caeca6c6c0c767e7d2f9baf
up1 <somkiat.p@gmail.com>
2023-08-24 14:37:32 +0700

    Use Stringer interface

--- day1/demo_struct.go
+++ day1/demo_struct.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type DayOfMonth int
 
 type Employee struct {
@@ -15,16 +17,19 @@ type EmployeeWithEmail struct {
 // Receiver
 // 1. Value Receiver
 // 2. Pointer Receiver
-func (e *Employee) leave() bool {
+func (e Employee) leave() bool {
 	e.Name = "Update somkiat"
 	return true
 }
 
+func (e Employee) String() string {
+	return "........."
+}
+
 func main() {
-	e := EmployeeWithEmail{}
+	e := Employee{}
 	e.leave()
-	e.Email = "email"
-	println(e.Name)
+	fmt.Println(e)
 
 	var d DayOfMonth = 5 // Day of month
 	println(d)
